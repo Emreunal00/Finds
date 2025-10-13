79333e121eebe69975c4c95f243209491a16270c
Emreunal00 <umutemre2004@gmail.com>
2025-10-13 22:51:18 +0300

    Başarılı ilk push

---
+++ FindsApp/FindsApp/FindsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindsAppApp.swift
+//  FindsApp
+//
+//  Created by Emre ünal on 13.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct FindsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
